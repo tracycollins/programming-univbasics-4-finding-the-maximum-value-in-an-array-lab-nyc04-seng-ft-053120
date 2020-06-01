66a7df197b353cc950e26e5e959e99793e698a2c
Tracy Collins <tc@threeceemedia.com>
2020-06-01 21:18:53 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,7 @@
 def find_max_value(array)
-  # Add your solution here
+  max_value = 0;
+  array.length.times{|index|
+    max_value =(array[index] > max_value) ? array[index] : max_value
+  }
+  max_value
 end
